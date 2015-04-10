d6342aaa1e9a5a4ba7ac5503bdcfd38adf82f6a2
JM Arbona <jeanmichel.arbona@gmail.com>
2015-04-10 15:33:25 +0200

    Set python module install right

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ files = ["template/*",
          "halley/*"]
 
 setup(name='cpolymer',
-      version='0.3',
+      version='0.33',
       description='Creating initial configuration for polymer (to work with lammps)',
       url='https://github.com/jeammimi/cpolymer',
       author='Jean-michel Arbona',
       author_email='jeanmichel.arbona@gmail.com',
       license='MIT',
+      package_data = {'cpolymer' : files },
       packages=['cpolymer'],
       zip_safe=False)
